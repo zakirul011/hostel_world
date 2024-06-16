423c5a5df44b678e42783830cf4480027b7060a9
Zakirul Islam <zakirul011@gmail.com>
2024-06-17 01:26:53 +0600

    Cleanup files and codes

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,101 +48,7 @@ headerArea.forEach((area) => {
 
 (function ($) {
   "use strict";
-
-  //========== MAGNIFIC POPUP ==========>
-  $(".video-btn").magnificPopup({
-    type: "iframe",
-  });
-  $(".pop-img-btn").magnificPopup({
-    type: "image",
-  });
-  $(".gallery-item a").magnificPopup({
-    type: "image",
-    gallery: {
-      enabled: true,
-    },
-  });
-  //========== MAGNIFIC POPUP// ==========>
-
-  //========== ONE PAGE NAV ==========>
-  var top_offset = $(".header-area").height() - 170;
-  $(".mainmenu ul, .sidebar ul").onePageNav({
-    scrollOffset: top_offset,
-  });
-  //========== ONE PAGE NAV// ==========>
-
-  //========== SKROLLR JS ==========>
-  var skrollrSetting = {
-    forceHeight: false,
-    smoothScrollingDuration: 500,
-  };
-  var s = skrollr.init(skrollrSetting);
-  function doubleAction() {
-    if (window.matchMedia("(max-width: 991px)").matches) {
-      s.destroy();
-    } else {
-      s = skrollr.init(skrollrSetting);
-    }
-  }
-  window.addEventListener("load", () => {
-    doubleAction();
-  });
-  window.addEventListener("resize", () => {
-    doubleAction();
-  });
-  //========== SKROLLR JS// ==========>
-
-  //========== HERO AREA SLIDER ==========>
-  function heroSlider() {
-    var BasicSlider = $(".hero-slider");
-    BasicSlider.on("init", function (e, slick) {
-      var $firstAnimatingElements = $(".hero-slide:first-child").find(
-        "[data-animation]"
-      );
-      doAnimations($firstAnimatingElements);
-    });
-    BasicSlider.on(
-      "beforeChange",
-      function (e, slick, currentSlide, nextSlide) {
-        var $animatingElements = $(
-          '.hero-slide[data-slick-index="' + nextSlide + '"]'
-        ).find("[data-animation]");
-        doAnimations($animatingElements);
-      }
-    );
-    BasicSlider.slick({
-      autoplay: false,
-      autoplaySpeed: 10000,
-      dots: false,
-      fade: true,
-      arrows: true,
-      appendArrows: ".hero-slider-wrap .slider-nav",
-      prevArrow:
-        '<button type="button" class="slick-prev"><i class="fal fa-angle-left"></i></button>',
-      nextArrow:
-        '<button type="button" class="slick-next"><i class="fal fa-angle-right"></i></button>',
-    });
-    function doAnimations(elements) {
-      var animationEndEvents =
-        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-      elements.each(function () {
-        var $this = $(this);
-        var $animationDelay = $this.data("delay");
-        var $animationType = "animated " + $this.data("animation");
-        $this.css({
-          "animation-delay": $animationDelay,
-          "-webkit-animation-delay": $animationDelay,
-        });
-        $this.addClass($animationType).one(animationEndEvents, function () {
-          $this.removeClass($animationType);
-        });
-      });
-    }
-  }
-  heroSlider();
-  //========== HERO AREA SLIDER// ==========>
-
-  //========== PRODUCT SLIDER ==========>
+  //========== FEED SLIDER ==========>
   $(".feed-slider").slick({
     slidesToShow: 5,
     slidesToScroll: 1,
@@ -178,5 +84,5 @@ headerArea.forEach((area) => {
       },
     ],
   });
-  //========== PRODUCT SLIDER// ==========>
+  //========== FEED SLIDER// ==========>
 })(jQuery);
